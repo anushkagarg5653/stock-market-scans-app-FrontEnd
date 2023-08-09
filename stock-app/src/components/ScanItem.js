ced183e08a3d19988cd375c1ee5ed9b73f7ef4e0
Anushka <anushkagarg0811@gmail.com>
2023-08-09 15:44:39 +0530

    mapping error resolved: added conditional checks when data is not available

--- stock-app/src/components/ScanItem.js
+++ stock-app/src/components/ScanItem.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import SubCriteria from './SubCriteria'
 
 const ScanItem = ({scan}) => {
+    if(!scan || !scan.subCriteria || scan.subCriteria.length === 0){
+        return null;
+    }
+    console.log(scan.name, 'SCAN');
   return (
     <div>
         <h2>{scan.name}</h2>
         {scan.subCriteria.map((subCriteria, index) => (
-            <subCriteria key={index} subCriteria={subCriteria} />
+            <SubCriteria key={index} subCriteria={subCriteria} />
         ))}
     </div>
   )
--- stock-app/src/components/ScanList.js
+++ stock-app/src/components/ScanList.js
@@ -1,18 +1,26 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import ScanItem from './ScanItem';
 
 const ScanList = () => {
-    const [scans, setScans] = ([]);
+    const [scans, setScans] = useState([]);
 
     useEffect(() =>{
-        fetch('/api/scans')
-        .then((response) =>response.json())
-        .then(data => setScans(data));
-        })
+        fetch('http://localhost:5000/api/scans')
+        .then(response => response.json())
+        .then(data => {
+            console.log("data",data);
+            setScans(data)})
+        .catch(error => console.error('error fetching', error));
+        }, []);
+
+        if(scans.length ===0){
+            return <p>Loading...</p>
+        }
   return (
 
     <div>
         <h1>Stock Market ScanList</h1>
-        {ScanList.map(scan => (
+        {scans.map(scan => (
             <ScanItem key = {scan.id} scan={scan}/>
             ))}
     </div>
